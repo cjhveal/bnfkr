09b396475cd5600b5350d6241960901a2e9e98b7
Cody Veal <cveal05@gmail.com>
2012-05-28 08:38:13 -0400

    specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,6 +2,10 @@ require 'rspec'
 require './bnfkr.rb'
 
 describe Parser do
+  before(:all) do
+    @parser = Parser.new
+  end
+
   describe "#read" do
     it "should strip out any non-bf instructions"
     it "should return an array of one character instructions"
@@ -14,12 +18,25 @@ describe Parser do
   describe "#normalize" do
     it "should keep the pointer within the array"
     it "should keep each cell within its limit"
-
   end
 
   describe "#reset" do
-    it "should set all values of array to 0"
-    it "should set all pointers to 0"
-    it "should set loop stack and operators list to empty arrays"
+    before(:all) do
+      @parser.send :reset
+    end
+
+    it "should set all values of array to 0" do
+      @parser.array.all? {|x| x == 0}.should be_true
+    end
+
+    it "should set all pointers to 0" do
+      @parser.pointer.should eq 0
+      @parser.op_pointer.should eq 0
+    end
+
+    it "should set loop stack and instructions list to empty arrays" do
+      @parser.loop_stack.empty?.should be_true
+      @parser.ops.empty?.should be_true
+    end
   end
 end
